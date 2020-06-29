c6f32f6c785544588aae8ef5fb37a9eff3aa1e88
forexms78 <qudgh9709@gmail.com>
2020-06-29 22:05:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tween_animation/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.tween_animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+    Button tran, rota, scal, alph;
+    View.OnClickListener cl;
+    Animation ani;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = (ImageView)findViewById(R.id.image);
+
+        tran = (Button)findViewById(R.id.translate);
+        rota = (Button)findViewById(R.id.rotate);
+        scal = (Button)findViewById(R.id.scale);
+        alph = (Button)findViewById(R.id.alpha);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.translate:
+                        //ani= new TranslateAnimation(0,400,0,400);     //픽셀형태로는 요즘에 잘 안쓰인다
+                        ani = new TranslateAnimation(
+                                Animation.RELATIVE_TO_SELF,0f, //시작 x축 위치
+                                Animation.RELATIVE_TO_PARENT,2f, //도착 x축 위치
+                                Animation.RELATIVE_TO_SELF,0f, //시작 y축 위치
+                                Animation.RELATIVE_TO_PARENT,2f  //도착 y축 위치
+                        );
+                        ani.setDuration(5000);
+                        iv.startAnimation(ani);
+                        break;
+                    case R.id.rotate:
+                        //ani = new RotateAnimation(0,-180);앱솔루트
+                        ani = new RotateAnimation(0,-360,
+                                Animation.RELATIVE_TO_SELF,1f,
+                                Animation.RELATIVE_TO_SELF,1f);
+                        ani.setRepeatCount(3);
+                        ani.setRepeatMode(Animation.REVERSE);
+                        ani.setDuration(3000);
+                        iv.startAnimation(ani);
+                        break;
+                    case R.id.scale:
+                        //ani = new ScaleAnimation(0f,1f,0f,1f);
+                        ani = new ScaleAnimation(1f,0f,1f,0f,
+                            Animation.RELATIVE_TO_SELF, 0.5f,
+                            Animation.RELATIVE_TO_SELF, 0.5f);
+                        ani.setDuration(1500);
+                        iv.setAnimation(ani);
+                        break;
+                    case R.id.alpha:
+                        ani = new AlphaAnimation(0f, 1f);
+                        ani.setRepeatCount(3);
+                        ani.setRepeatMode(Animation.REVERSE);
+                        ani.setDuration(4000);
+                        iv.setAnimation(ani);
+                        break;
+
+                }
+            }
+        };
+        tran.setOnClickListener(cl);
+        rota.setOnClickListener(cl);
+        scal.setOnClickListener(cl);
+        alph.setOnClickListener(cl);
+    }
+}
